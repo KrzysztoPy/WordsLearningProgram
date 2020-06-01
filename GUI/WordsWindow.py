6260b9fb7a369bbbfd77e23812b08fd092235c10
Kriss <geraltrivia19@gmail.com>
2020-06-01 14:07:36 +0200

    Load file to table-> Problem with for in set_words_from_file

--- GUI/WordsWindow.py
+++ GUI/WordsWindow.py
@@ -1,6 +1,6 @@
-from tkinter import *
 from tkinter import messagebox
 from LOGIC.Logic import *
+from tkinter import ttk
 
 
 class WordsWindow(Logic):
@@ -20,6 +20,8 @@ class WordsWindow(Logic):
 
     clicked_file = None
     choice_file = None
+
+    table = None
 
     # Set position and size all windows
     def set_main_pop(self, main_root) -> str:
@@ -46,37 +48,40 @@ class WordsWindow(Logic):
         #
         self.words_window_label()
         self.words_window_in_field()
-
-        self.words_window_button()
         self.option_menu()
+        self.words_window_button()
 
         # Set windows close event
         self.words_window_root.protocol("WM_DELETE_WINDOW", self.close_words_window)
+        self.set_table()
 
     # set all button
     def words_window_button(self):
 
-        butt_save = Button(self.words_window_root, text="Save",
+        butt_save = Button(self.words_window_root, text="Save word list",
                            command=lambda: [self.info_popup(self.logic.file_create(self.file_name.get()))
                                , self.logic.file_list(), self.option_menu()])
         butt_save.grid(row=1, column=2, ipadx=70, pady=0)
         self.words_window_root.grid_columnconfigure(butt_save, minsize=1)
 
-        butt_load = Button(self.words_window_root, text="Load")
+        butt_load = Button(self.words_window_root, text="Load", command=lambda: self.set_value_table(self.logic.load_butt(self.clicked_file.get())))
         butt_load.grid(row=1, column=4, ipadx=70, pady=0)
         self.words_window_root.grid_columnconfigure(butt_load, minsize=1)
 
-        butt_load = Button(self.words_window_root, text="Add")
-        butt_load.grid(row=2, column=4, ipadx=10, pady=25)
+        butt_save = Button(self.words_window_root, text="Save words")  # Without command!!!
+        butt_save.grid(row=4, column=4, ipadx=70, pady=0)
 
-        butt_load = Button(self.words_window_root, text="Remove")
-        butt_load.grid(row=2, column=5, ipadx=10, pady=25)
+        butt_add = Button(self.words_window_root, text="Add")
+        butt_add.grid(row=2, column=4, ipadx=10, pady=25)
 
-        butt_load = Button(self.words_window_root, text="Find")
-        butt_load.grid(row=2, column=6, ipadx=10, pady=25)
+        butt_remove = Button(self.words_window_root, text="Remove")
+        butt_remove.grid(row=2, column=5, ipadx=10, pady=25)
+
+        butt_find = Button(self.words_window_root, text="Find")
+        butt_find.grid(row=2, column=6, ipadx=10, pady=25)
 
         butt_back = Button(self.words_window_root, text="Back", command=self.close_words_window)
-        butt_back.grid(row=5, column=0, ipadx=25, pady=25)
+        butt_back.grid(row=4, column=0, ipadx=25, pady=25)
 
     # set all field
     def words_window_in_field(self):
@@ -93,9 +98,10 @@ class WordsWindow(Logic):
     # Set all label
     def words_window_label(self):
         currently_open_file = Label(self.words_window_root,
-                                    text="Directory with word lists {}  ".format(self.logic.dir_name),
+                                    text="Directory with word lists: {}                       ".format(
+                                        self.logic.dir_name),
                                     fg="red", font='Helvetica 15 bold', padx=2)
-        currently_open_file.grid(row=0, column=0, columnspan=6)
+        currently_open_file.grid(row=0, column=0, columnspan=4)
 
         label_set_name_file = Label(self.words_window_root, text="Choice words list name ")
         label_set_name_file.grid(row=1, column=0)
@@ -107,7 +113,30 @@ class WordsWindow(Logic):
         lab_english_word.grid(row=2, column=2)
 
         lab_english_word = Label(self.words_window_root, text="Selected file")
-        lab_english_word.grid(row=0, column=4)
+        lab_english_word.grid(row=0, column=3)
+
+    def set_table(self):
+        self.table = ttk.Treeview(self.words_window_root, selectmode='browse')
+        self.table.grid(row=3, column=2, columnspan=2)
+        verscrlbar = ttk.Scrollbar(self.words_window_root, orient="vertical", command=self.table.yview)
+        verscrlbar.grid(row=3, column=2, sticky=N + S + E, columnspan=2)
+        self.table.configure(xscrollcommand=verscrlbar.set)
+
+        self.table["columns"] = ("1", "2", "3")
+        self.table['show'] = 'headings'
+
+        self.table.column("1", width=20, anchor='se')
+        self.table.column("2", width=180, anchor='se')
+        self.table.column("3", width=180, anchor='se')
+
+        self.table.heading("1", text="Id")
+        self.table.heading("2", text="Polish word")
+        self.table.heading("3", text="English word")
+
+    def set_value_table(self, words):
+        # [[x,y,x],[x,y,z]]
+        for i in words:
+            self.table.insert("", 'end', text="L1", values=(i[0], i[1], i[2]))
 
     # Give possibility to choice
     def option_menu(self):
--- LOGIC/Logic.py
+++ LOGIC/Logic.py
@@ -5,8 +5,9 @@ import os
 
 
 class Logic:
+    # Create dir where save your list words
     dir_name = "Words list"
-    polish_english_words = []
+    # Stores a list of files
     list_all_file = []
 
     # Create directory when you save new words list
@@ -40,7 +41,7 @@ class Logic:
             create.write(os.path.dirname(os.path.realpath(__file__)))
             create.close()
 
-    def wordslist_path(self):
+    def words_list_path(self):
         tmp_path = os.path.realpath(__file__)
 
         for i in tmp_path[::-1]:
@@ -53,18 +54,18 @@ class Logic:
         return tmp_path
 
     def create_file(self, file_name):
-        tmp = self.wordslist_path() + "\\" + file_name
-        open(tmp, "a").close()
+        tmp_path = self.words_list_path() + "\\" + file_name
+        open(tmp_path, "a").close()
         return ["Information", "The file has been created"]
 
     def check_have_txt(self, file_name):
-        tmp = ""
+        tmp_path = ""
         file_name = file_name[::-1]
         if len(file_name) > 4:
             for i in range(0, 5):
-                tmp += file_name[i]
+                tmp_path += file_name[i]
             file_name = file_name[::-1]
-            if tmp == ".txt":
+            if tmp_path == ".txt":
                 return file_name
             else:
                 return file_name + ".txt"
@@ -73,7 +74,7 @@ class Logic:
             return file_name + ".txt"
 
     def file_list(self):
-        self.list_all_file = os.listdir(self.wordslist_path())
+        self.list_all_file = os.listdir(self.words_list_path())
 
         if self.list_all_file.__len__() == 0:
             self.list_all_file.append("Empty")
@@ -84,16 +85,45 @@ class Logic:
             return ["Error", "File name can't be empty."]
         elif file_name == ".txt":
             return ["Error", "Prohibited file name: .txt"]
-        file_path = self.wordslist_path() + "\\" + self.check_have_txt(file_name)
+        file_path = self.words_list_path() + "\\" + self.check_have_txt(file_name)
         if os.path.exists(file_path):
             return ["Error", "A file with this name already exists.Choice different name. "]
         else:
             return self.create_file(self.check_have_txt(file_name))
 
+    def load_butt(self, file_name):
+        tmp_path = self.words_list_path() + "\\" + file_name
+        list_words = open(tmp_path, "r", encoding="utf=8")
+        file_data = list_words.read()
+        return self.set_words_from_file(file_data)
 
+    def set_words_from_file(self, file_data):
+        words_list = []
+        flag = FALSE
+        # dom,ojczyzna|home
+        print(file_data.__len__())
+        for i in file_data.__len__():  # Błąd nie wchodzi do fora sprawdzić
+            polish_words, english_words = ""
+            for j in words_list[i]:
+                if j != "|" and flag == FALSE:
+                    if j == ",":
+                        polish_words += j + " "
+                    else:
+                        polish_words += j
+                elif j == "|":
+                    flag = TRUE
+                elif j != "|" and flag == True:
+                    if j == ",":
+                        english_words += j + " "
+                    else:
+                        english_words += j
+                words_list += [i, polish_words, english_words]
+        return words_list.copy()
 
 
 def test():
+    # logic = Logic()
+    # logic.load_butt("bla.txt")
     # path = os.path.realpath(__file__)
     # print(os.path.basename(__file__))
     # print("\\")
