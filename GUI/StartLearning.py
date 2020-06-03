b4c419a8ec97da91e9ab3b249ceece19a25412a8
Kriss <geraltrivia19@gmail.com>
2020-06-03 13:48:56 +0200

    End GUI

--- GUI/StartLearning.py
+++ GUI/StartLearning.py
@@ -10,13 +10,20 @@ class StartLearning(LearningLogic):
     # StartLearning root
     learning_root = None
 
+    # Sentence and words list
     words_list = []
-    sent_list = []
+    sentence_list = []
+
+    # for change button version name
+    version_label_text = None
+    word_sent_version_chose = None
 
     # Main def
     def start_learning_menu(self, root):
         # set var root from main root
         self.main_root = root
+
+        self.version_label_text = StringVar()
 
         self.learning_root = Toplevel(self.main_root)
         # Set window size
@@ -32,10 +39,10 @@ class StartLearning(LearningLogic):
         # Set Label
         self.learning_label()
 
+        self.learning_field()
         #
         self.learning_butt()
         #
-        self.learning_field()
 
     def learning_combo_box(self):
         file_lists = self.l_logic.file_list()
@@ -43,17 +50,26 @@ class StartLearning(LearningLogic):
         clicked_file.set(file_lists[0])
 
         choice_file = OptionMenu(self.learning_root, clicked_file, *file_lists)
-        choice_file.grid(row=1, column=1, ipadx=65)
+        choice_file.grid(row=1, column=0, ipadx=65)
 
     def learning_label(self):
         # Empty label
         empty_before_report_label = Label(self.learning_root, text="")
         empty_before_report_label.grid(row=2, column=0, ipadx=65)
         empty_after_report_label = Label(self.learning_root, text="")
+        empty_after_report_label.grid(row=3, column=0, ipadx=65)
+
+        empty_after_report_label = Label(self.learning_root, text="")
+        empty_after_report_label.grid(row=3, column=0, ipadx=65)
+
+        empty_after_report_label = Label(self.learning_root, text="")
         empty_after_report_label.grid(row=4, column=0, ipadx=65)
 
         empty_after_report_label = Label(self.learning_root, text="")
         empty_after_report_label.grid(row=6, column=0, ipadx=65)
+
+        empty_after_report_label = Label(self.learning_root, text="")
+        empty_after_report_label.grid(row=8, column=0, ipadx=65)
 
         empty_after_report_label = Label(self.learning_root, text="")
         empty_after_report_label.grid(row=10, column=0, ipadx=65)
@@ -67,14 +83,48 @@ class StartLearning(LearningLogic):
         # /#
 
         set_file = Label(self.learning_root, font='Helvetica 11 bold', text="Choice words list name ")
-        set_file.grid(row=2, column=1)
-        set_file = Label(self.learning_root, font='Helvetica 11 bold', text="Choice words list name ")
-        set_file.grid(row=2, column=3)
+        set_file.grid(row=0, column=0)
 
-        set_file = Label(self.learning_root, font='Helvetica 11 bold', text="Choice words list name ")
-        set_file.grid(row=0, column=1)
+        word_sent_version = Label(self.learning_root, font='Helvetica 11 bold',
+                                  text="Answer in language: ")
+        word_sent_version.grid(row=1, column=2)
 
-        # General report label
+        self.word_sent_version_chose = Label(self.learning_root, font='Helvetica 11 bold',
+                                             text="Polish version")
+        self.word_sent_version_chose.grid(row=1, column=3)
+
+        # From file text word
+        file_words_label = Label(self.learning_root, font='Helvetica 11 bold', text="Words: ")
+        file_words_label.grid(row=4, column=1)
+
+        file_get_words_label = Label(self.learning_root, fg="green", font='Helvetica 11 bold', text="No list selected ")
+        file_get_words_label.grid(row=4, column=2)
+        # /#
+
+        # Words label
+        label_polish_word = Label(self.learning_root, font='Helvetica 11 bold', text="Polish version ---> ")
+        label_polish_word.grid(row=5, column=0)
+
+        label_english_word = Label(self.learning_root, font='Helvetica 11 bold', text="English version ---> ")
+        label_english_word.grid(row=5, column=2)
+        # /#
+
+        # From file text sentence
+        file_words_label = Label(self.learning_root, font='Helvetica 11 bold', text="Sentence: ")
+        file_words_label.grid(row=7, column=1)
+
+        file_get_words_label = Label(self.learning_root, fg="green", font='Helvetica 11 bold', text="No list selected ")
+        file_get_words_label.grid(row=7, column=2)
+        # /#
+
+        # Sentence label
+        label_polish_sentence = Label(self.learning_root, font='Helvetica 11 bold', text="Polish sentence version ")
+        label_polish_sentence.grid(row=9, column=0)
+
+        label_english_sentence = Label(self.learning_root, font='Helvetica 11 bold', text="English sentence version ")
+        label_english_sentence.grid(row=11, column=0)
+
+        # Words report label
         rap_count_word = Label(self.learning_root, font='Helvetica 11 bold', text="Words report ->>> ")
         rap_count_word.grid(row=12, column=0)
 
@@ -114,46 +164,58 @@ class StartLearning(LearningLogic):
                                        text="Sentence completed ---> ")
         sent_rap_when_complied.grid(row=17, column=4)
 
-        # Words label
-        label_polish_word = Label(self.learning_root, font='Helvetica 11 bold', text="Polish version ---> ")
-        label_polish_word.grid(row=3, column=0)
+    def variable_labels(self):
+        self.word_sent_version_chose = Label(self.learning_root, font='Helvetica 11 bold',
+                                             textvariable=self.version_label_text)
+        self.word_sent_version_chose.grid(row=1, column=3)
 
-        label_english_word = Label(self.learning_root, font='Helvetica 11 bold', text="English version ---> ")
-        label_english_word.grid(row=3, column=2)
-        # /#
+        # word_sent_version_chose = Label(self.learning_root, font='Helvetica 11 bold',
+        #                                 textvariable=self.version_label_text)
+        # word_sent_version_chose.grid(row=1, column=3)
 
-        # Sentence label
-        label_polish_sentence = Label(self.learning_root, font='Helvetica 11 bold', text="Polish sentence version ")
-        label_polish_sentence.grid(row=8, column=0)
-
-        label_english_sentence = Label(self.learning_root, font='Helvetica 11 bold', text="English sentence version ")
-        label_english_sentence.grid(row=9, column=0)
+        # set_file = Label(self.learning_root, font='Helvetica 11 bold', text="No list selected")
+        # set_file.grid(row=4, column=1)
+        #
+        # set_file = Label(self.learning_root, font='Helvetica 11 bold', text="Sentence: ")
+        # set_file.grid(row=0, column=1)
 
     def learning_butt(self):
         butt_load = Button(self.learning_root, font='Helvetica 11 bold', text="Load")
         # ,command=lambda: [self.set_table(),self.set_value_table(self.logic.load_butt(self.clicked_file.get()))])
-        butt_load.grid(row=1, column=2, ipadx=70, pady=0)
+        butt_load.grid(row=1, column=1, ipadx=70, pady=0)
+
+        version_butt = Button(self.learning_root, font='Helvetica 11 bold', text="Change",
+                              command=lambda: [self.version_label_text.set(self.l_logic.set_version_label()),
+                                               self.variable_labels()])
+
+        # ,command=lambda: [self.set_table(),self.set_value_table(self.logic.load_butt(self.clicked_file.get()))])
+        version_butt.grid(row=1, column=4, ipadx=70, pady=0)
 
         butt_word_check = Button(self.learning_root, font='Helvetica 11 bold', text="Words Check")
         # ,command=lambda: [self.set_table(),self.set_value_table(self.logic.load_butt(self.clicked_file.get()))])
-        butt_word_check.grid(row=3, column=4, ipadx=70, pady=0)
+        butt_word_check.grid(row=5, column=4, ipadx=70, pady=0)
 
         butt_sentence_check = Button(self.learning_root, font='Helvetica 11 bold',
                                      text="Sentence Check")
         # ,command=lambda: [self.set_table(),self.set_value_table(self.logic.load_butt(self.clicked_file.get()))])
-        butt_sentence_check.grid(row=9, column=4, ipadx=70, pady=0)
+        butt_sentence_check.grid(row=11, column=4, ipadx=70, pady=0)
+
+        butt_back = Button(self.learning_root, font='Helvetica 11 bold',
+                           text="BACK", command=self.l_logic.close_Learning_window)
+        # ,command=lambda: [self.set_table(),self.set_value_table(self.logic.load_butt(self.clicked_file.get()))])
+        butt_back.grid(row=18, column=0, ipadx=70, pady=0)
 
     def learning_field(self):
         # Words Label
         entry_polish_word = Entry(self.learning_root, width=25, bd=5)
-        entry_polish_word.grid(row=3, column=1, ipadx=25, pady=25)
+        entry_polish_word.grid(row=5, column=1, ipadx=25, pady=25)
 
         entry_english_word = Entry(self.learning_root, width=25, bd=5)
-        entry_english_word.grid(row=3, column=3, ipadx=25, pady=25)
+        entry_english_word.grid(row=5, column=3, ipadx=25, pady=25)
         # /#
 
         entry_polish_sentence = Entry(self.learning_root, width=25, bd=5)
-        entry_polish_sentence.grid(row=8, column=1, ipadx=225, pady=25, columnspan=3)
+        entry_polish_sentence.grid(row=9, column=1, ipadx=225, pady=25, columnspan=3)
 
         entry_english_sentence = Entry(self.learning_root, width=25, bd=5)
-        entry_english_sentence.grid(row=9, column=1, ipadx=225, pady=25, columnspan=3)
+        entry_english_sentence.grid(row=11, column=1, ipadx=225, pady=25, columnspan=3)
--- LOGIC/LearningLogic.py
+++ LOGIC/LearningLogic.py
@@ -7,10 +7,14 @@ class LearningLogic:
     # Directory name where is all file
     dir_name = "Words list"
 
+    # Button English/Polish version
+    label_version = ["English version", "Polish version"]
+    label_version_flag = False
+
     def set_main_pop(self, main_root):
         # Default window size
         window_width = 1200
-        window_height = 600
+        window_height = 620
         # Set center position
         position_right = int(main_root.winfo_screenwidth() / 2 - window_width / 2)
         position_down = int(main_root.winfo_screenheight() / 2 - window_height / 2)
@@ -35,5 +39,17 @@ class LearningLogic:
             self.file_list.append("Empty")
         return self.file_list
 
+    def set_version_label(self) -> str:
+        if not self.label_version_flag:
+            self.label_version_flag = True
+            return self.label_version[0]
+        elif self.label_version_flag:
+            self.label_version_flag = False
+            return self.label_version[1]
+
     def load_file_butt(self):
         pass
+
+    def close_Learning_window(self):
+        self.words_window_root.destroy()
+        self.main_root.destroy()
